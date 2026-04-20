97b8a94fae168fec93c6178e46a70378798e6ed6
Alexandr Drozd <alxdrzd.development@gmail.com>
2026-04-20 21:25:26 +0300

    current progress

--- restaurant_visitor_eda/plots.py
+++ restaurant_visitor_eda/plots.py
@@ -11,6 +11,7 @@ app = typer.Typer()
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 def plot_target_distribution(data):
     fig, axes = plt.subplots(2, 2, figsize=(15, 10))
@@ -38,6 +39,77 @@ def plot_target_distribution(data):
 
     
     sns.despine()
+    plt.tight_layout()
+    plt.show()
+
+
+def build_barplot_for_air_genres(data):
+    df_air_genre_name = data.air_genre_name.value_counts()
+    plt.figure(figsize=(12, 8))
+    sns.set_style("whitegrid")
+    ax = sns.barplot(data=df_air_genre_name, orient='h', palette='viridis')
+    for container in ax.containers:
+        ax.bar_label(container, padding=3)
+    plt.title('air restaurants')
+    sns.despine()
+    plt.show()
+
+def build_barplot_for_hpg_genres(data):
+    df_hpg_genre_name = data.hpg_genre_name.value_counts()
+    plt.figure(figsize=(12, 8))
+    sns.set_style("whitegrid")
+    ax = sns.barplot(data=df_hpg_genre_name, orient='h', palette = 'viridis')
+    for container in ax.containers:
+        ax.bar_label(container, padding=3)
+    plt.title('hpg restaurants')
+    sns.despine()
+    plt.show()
+
+def plot_median_visitors_per_genre(df: pd.DataFrame):
+    genre_stats = (
+        df.groupby('air_genre_name')['visitors']
+        .median()
+        .sort_values(ascending=False)
+        .reset_index()
+    )
+
+    plt.figure(figsize=(12, 8))
+    sns.set_style("whitegrid")
+
+    ax = sns.barplot(
+        data=genre_stats, 
+        x='visitors', 
+        y='air_genre_name', 
+        palette='viridis'
+    )
+
+    for container in ax.containers:
+        ax.bar_label(container, padding=3)
+    plt.title('Median Visitors by Restaurant Genre', fontsize=16, pad=20)
+    plt.xlabel('Median Daily Visitors', fontsize=12)
+    plt.ylabel('Genre', fontsize=12)
+    
+    sns.despine(left=True, bottom=True) 
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+def plot_visitors_boxplot_air(df: pd.DataFrame):
+    sorted_idx = (
+        df.groupby('air_genre_name')['visitors']
+        .median()
+        .sort_values(ascending=False)
+        .index
+    )
+
+    sns.boxplot(data=df, x='visitors', y='air_genre_name', order=sorted_idx, palette='viridis', hue='air_genre_name', legend=False)
+    plt.xscale('log')
+    plt.grid(True, which="both", ls="-", alpha=0.2) # Добавим сетку для логарифмов
+    plt.title('Distribution of Visitors by Genre (Log Scale)')
+    plt.xlabel('Visitors (log scale)')
+    plt.ylabel('Genre')
     plt.tight_layout()
     plt.show()
 
